3b2db67a8564a75c080db03b5c6aa42ff653faa3
Cesar Aramayo <cesar_aramayo@hotmail.es>
2022-02-05 15:26:38 -0300

    modificando la clase Cliente con sus atributos

--- src/app/clientes/cliente.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Cleinte } from './cliente';
-
-describe('Cleinte', () => {
-  it('should create an instance', () => {
-    expect(new Cleinte()).toBeTruthy();
-  });
-});
--- src/app/clientes/cliente.ts
+++ src/app/clientes/cliente.ts
@@ -1,2 +1,7 @@
 export class Cleinte {
+  id: number;
+  nombre: string;
+  apellido: string;
+  createAt: string;
+  email: string;
 }
