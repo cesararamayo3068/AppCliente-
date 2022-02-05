f61df492cde155aaeb9e1885cdfa762537b700ec
Cesar Aramayo <cesar_aramayo@hotmail.es>
2022-02-05 14:46:57 -0300

    creando componente header y clase Cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso de Angular con Spring Boot';
+  profesor: string = 'Andres Guzman';
 }
---
+++ src/app/clientes/cliente.spec.ts
@@ -0,0 +1,7 @@
+import { Cleinte } from './cliente';
+
+describe('Cleinte', () => {
+  it('should create an instance', () => {
+    expect(new Cleinte()).toBeTruthy();
+  });
+});
