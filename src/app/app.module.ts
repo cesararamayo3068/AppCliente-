1220898511d6a228c4cdf95adf2a70ba9c1db106
Cesar Aramayo <cesar_aramayo@hotmail.es>
2022-02-05 17:52:30 -0300

    agregando rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { HeaderComponent } from './header/header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/clientes', pathMatch: 'full' },
+  { path: 'clientes', component: ClientesComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientesComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+  BrowserModule,
+  RouterModule.forRoot(routes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ClienteService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
